ab312adb35d16cfafd60c650449562ffd4942c55
jffarge <jf@dagger.io>
2023-07-26 17:33:44 +0200

    Update the title
    Signed-off-by: jffarge <jf@dagger.io>

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Dagger toto
+          Learn Dagger tata
         </a>
       </header>
     </div>
