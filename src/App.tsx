1882b7b12ed720d52017353977167f720657f615
jffarge <jf@dagger.io>
2023-07-27 16:20:07 +0200

    Update title
    Signed-off-by: jffarge <jf@dagger.io>

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Do not learn Dagger.
+          Learn React
         </a>
       </header>
     </div>
