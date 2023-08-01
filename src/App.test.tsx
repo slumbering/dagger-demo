d12ad6d98020959d2731e523b79eeade35b50c4c
Jffarge <jf@dagger.io>
2023-08-01 23:02:07 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//tests
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Learn Dagger/i);
